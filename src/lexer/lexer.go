8430a5e4e4e9b6def8bd95de89c8f8e06739318d
sappharx <vinnysisk@gmail.com>
2017-03-21 21:03:46 -0600

    add more keywords and support for 2 character tokens

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -23,13 +23,27 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{ Type: token.EQ, Literal: literal }
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{ Type: token.NOT_EQ, Literal: literal }
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -94,6 +108,15 @@ func (l *Lexer) readNumber() string {
 	return l.input[position : l.position]
 }
 
+func (l *Lexer) readTwoCharToken() string {
+	position := l.position
+
+	l.position = l.readPosition
+	l.readPosition += 1
+
+	return l.input[position : l.readPosition]
+}
+
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -103,6 +126,14 @@ func (l *Lexer) readChar() {
 
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func (l *Lexer) skipWhitespace() {
